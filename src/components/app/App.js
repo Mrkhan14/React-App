1fe3db7c6b6ddf8f1446312be3288fb1154fbe28
Mrkhan14 <aabdulaxadkhan@gmail.com>
2024-02-13 14:53:54 +0500

    #29. useRef hook

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,149 +1,24 @@
 import './App.css';
-import {Component, useState, useEffect, useCallback, useMemo} from "react";
-// import CounterItem from "../counter-item/counter-item";
-
-// class User extends Component{
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             counter: 0,
-//             age: ''
-//         }
-//     }
-//
-//     clickHander = () =>{
-//         // bu bizga yangi qimat qaytaradi
-//         this.setState(prevState =>({
-//             counter: prevState.counter + 1,
-//         }))
-//     }
-//     clickHander2 = () =>{
-//         // bu bizga yangi qimat qaytaradi
-//         this.setState(prevState =>({
-//             counter: prevState.counter - 1,
-//         }))
-//     }
-//
-//     clickHander3 = () =>{
-//         // bu bizga yangi qimat qaytaradi
-//         this.setState({
-//             counter: 0
-//         })
-//     }
-//     changeHander =(e, name) =>{
-//         console.log(name)
-//         this.setState({
-//             counter: e.target.value
-//         })
-//     }
-// 	componentDidMount() {
-// 		console.log("Mount")
-// 	}
-//
-// 	componentDidUpdate() {
-// 		console.log("Update")
-// 	}
-//
-//     componentWillUnmount() {
-//         console.log("Unmount")
-//     }
-//
-//     render() {
-//         const {firstName, lastNam, link,} = this.props
-//         // console.log(this.props)
-//
-//         const {age, counter} = this.state
-//
-//         return (
-//             <div className="border w-25 p-3 m-auto mb-4">
-//                 <h4>
-//                     Mening ismim - {firstName}, sharifim- {lastNam}
-//                     <br/>
-//                     Yoshim {age}
-//                 </h4>
-//                 <a href={link}>Link</a>
-//                 <div>
-//                     <button
-//                         onClick={this.clickHander}
-//                         type="button"
-//                         className="btn btn-success mx-2"
-//                     >
-//                         Success
-//                     </button>
-//                     <button
-//                         type="button"
-//                         className="btn btn-danger mx-2"
-// 						onClick={this.clickHander2}
-//                     >
-//                         Danger
-//                     </button>
-//                     <button
-//                         type="button"
-//                         className="btn btn-warning mx-2"
-//                         onClick={this.clickHander3}
-//                     >
-//                         Warning
-//                     </button>
-//                     <p>{counter}</p>
-//                     <div>
-//                         <label htmlFor="basic-url" className="form-label">Yoshim</label>
-//                         <input className="form-control" type="number" onChange={(e) => this.changeHander(e, 'Khan')}/>
-//                     </div>
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
-
-const bigCounter = (number) => {
-	console.log('reder')
-	let i = 0
-	while (i < 1000000000) i++
-	return number * 2
-}
-
+import {Component, useState, useEffect, useCallback, useMemo, useRef} from "react";
 const User = () =>{
-	const [counter, setCounter] = useState(0)
-	const [activ, setActiv] = useState(true)
+	const [cardNumber, setCardNumber] = useState('')
 
-	const onIncrement = () =>{
-		setCounter(counter + 1)
+	const cvcRef = useRef(null)
+
+	const handleInput = (e) =>{
+		const val = e.target.value
+		setCardNumber(val)
+		if (val.length === 16){
+			cvcRef.current.focus()
+		}
+		console.log(val.length)
 	}
-
-	const onTogagle = () => {
-		return setActiv(prevState => ! prevState)
-	}
-
-	// const counterDeneret = useCallback(() => {
-	// 	return new Array(counter).fill('').map((_, idx) => `Counter nomer ${idx + 1}`)
-	// 	},
-	// [counter]
-	// )
-
-	const colors={
-		color: activ ? 'green' : 'red',
-	}
-
-	const number = useMemo(()=> bigCounter(counter), [counter])
-
 	return (
 		<div className="border w-25 p-3 m-auto mb-4">
 
-			<div className="text-center" style={colors}>
-				user Actio
-				<b>{number}</b>
-			</div>
+			<input type='text' className='form-control mt-3' placeholder='Card name' onChange={handleInput} value={cardNumber} />
+			<input ref={cvcRef} type='text' className='form-control mt-3' placeholder='Secri name'/>
 
-			<div className="mt-5">
-				<button onClick={onIncrement} className="btn btn-success mx-2">
-					Increment
-				</button>
-				<button onClick={onTogagle} className="btn btn-success mx-2">
-					Togagle
-				</button>
-			</div>
-
-			{/*<CounterItem  counterDeneretY={counterDeneret}/>*/}
 		</div>
 	)
 }
